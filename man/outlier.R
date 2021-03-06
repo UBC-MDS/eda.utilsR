bf65fc3871756d582df29ab3b2e47582fca1e15b
Jiacheng <wangjc640@gmail.com>
2021-03-06 15:59:28 -0500

    docstring for outlier

---
+++ man/outlier.R
@@ -0,0 +1,23 @@
+#' A function that identify by z-test with threshold of 3, and deal with outliers based on the method the user choose.
+#'
+#' @param dataframe The target dataframe where the function is performed.
+#' @param columns A vector of the target columns where the function needed to be performed. Defualt is NULL, the function will check all columns
+#' @param method The method of dealing with outliers. Defualt is "trim"  
+#           - if "trim" : we completely remove data points that are outliers.
+#           - if "median" : we replace outliers with median values
+#           - if "mean" : we replace outliers with mean values
+#'
+#' @return dataframe object; a dataframe which the outlier has already process by the chosen method
+#' @export
+#'
+#' @examples
+#' data = data.Frame({  'SepalLengthCm':[5.1, 4.9, 4.7]
+#                         'SepalWidthCm':[1.4, 1.4, 99],
+#                          'PetalWidthCm:[0.2, 0.2, 0.2]})
+#'  
+#' outlier_identifier(data)
+#' 
+outlier_identifier <- function(dataframe, columns= NULL, method = 'trim'){
+    #TODO
+} 
+
