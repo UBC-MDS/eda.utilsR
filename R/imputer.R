3111c379e91649e2458d458ba4e80fed2cb77cdc
Chuang Wang <chuangw.sde@gmail.com>
2021-03-12 01:33:50 -0800

    finish tests and implementation for imputer function

--- R/imputer.R
+++ R/imputer.R
@@ -17,7 +17,7 @@ library(tibble)
 #' imputer(df)
 imputer <- function(df, strategy = "mean", fill_value = NA) {
   if(!is.data.frame(df)) (
-    stop("The ttt object to impute must be of 'data.frame' type")
+    stop("The object to impute must be of 'data.frame' type")
   )
   if(!is.character(strategy))(
     stop("strategy must be of type 'character'")
@@ -31,20 +31,36 @@ imputer <- function(df, strategy = "mean", fill_value = NA) {
   if(strategy != "constant" & is.numeric(fill_value))(
     stop("fill_value can be a number only when strategy is 'constant'")
   )
-  NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
-  NA2median <- function(x) replace(x, is.na(x), median(x, na.rm = TRUE))
-  NA2constant <- function(x) replace(x, is.na(x), fill_value)
-  NA2mostfreq <- function(x) replace(x, is.na(x), mode(x, na.rm = TRUE))
-  if(strategy == "mean") {
-    replace(data, TRUE, lapply(data, NA2mean))
-  } else if(strategy == "median") {
-    replace(data, TRUE, lapply(data, NA2median))
-  } else if (strategy == "most_frequent") {
-    replace(data, TRUE, lapply(data, NA2mostfreq))
-  } else if (strategy == "constant") {
-    replace(data, TRUE, lapply(data, NA2constant))
-  } else {
-    stop("strategy should be one of 'mean', 'median', 'most_frequent' and 'constant'")
+  # NA2mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE))
+  # NA2median <- function(x) replace(x, is.na(x), median(x, na.rm = TRUE))
+  # NA2constant <- function(x) replace(x, is.na(x), fill_value)
+  # NA2mostfreq <- function(x) replace(x, is.na(x), mode(x, na.rm = TRUE))
+  # if(strategy == "mean") {
+  #   replace(data, TRUE, lapply(data, NA2mean))
+  # } else if(strategy == "median") {
+  #   replace(data, TRUE, lapply(data, NA2median))
+  # } else if (strategy == "most_frequent") {
+  #   replace(data, TRUE, lapply(data, NA2mostfreq))
+  # } else if (strategy == "constant") {
+  #   replace(data, TRUE, lapply(data, NA2constant))
+  # } else {
+  #   stop("strategy should be one of 'mean', 'median', 'most_frequent' and 'constant'")
+  # }
+  for(i in 1:ncol(df)){
+    if (strategy == "mean") {
+      df[is.na(df[,i]), i] <- mean(df[,i], na.rm = TRUE)
+    } else if (strategy == "median") {
+      df[is.na(df[,i]), i] <- median(df[,i], na.rm = TRUE)
+    } else if (strategy == "most_frequent") {
+      df[is.na(df[,i]), i] <- as.numeric(names(which.max(table(df[,i]))))
+    } else if (strategy == "constant") {
+      df[is.na(df[,i]), i] <- fill_value
+    } else {
+      stop("strategy should be one of 'mean', 'median', 'most_frequent' and 'constant'")
+    }
+    
   }
+  
+  df
     
 }
--- tests/testthat/test.imputer.R
+++ tests/testthat/test.imputer.R
@@ -5,27 +5,27 @@ data = data.frame(
 )
 
 imp_mean = data.frame(
-  col1 = c(5.0, 4.0, 4.0, 7.0),
-  col2 = c(2.0, 2.0, 2.0, 2.0),
-  col3 = c(3.0, 5.0, 6.0, 6.0)
+  col1 = c(5, 4, 4, 7),
+  col2 = c(2, 2, 2, 2),
+  col3 = c(3, 5, 6, 6)
 )
 
 imp_median = data.frame(
-  col1 = c(4.0, 4.0, 4.0, 7.0),
-  col2 = c(2.0, 2.0, 2.0, 2.0),
-  col3 = c(3.0, 6.0, 6.0, 6.0)
+  col1 = c(4, 4, 4, 7),
+  col2 = c(2, 2, 2, 2),
+  col3 = c(3, 6, 6, 6)
 )
 
 imp_most_frequent = data.frame(
-  col1 = c(4.0, 4.0, 4.0, 7.0),
-  col2 = c(2.0, 2.0, 2.0, 2.0),
-  col3 = c(3.0, 6.0, 6.0, 6.0)
+  col1 = c(4, 4, 4, 7),
+  col2 = c(2, 2, 2, 2),
+  col3 = c(3, 6, 6, 6)
 )
 
 imp_constant = data.frame(
-  col1 = c(1.0, 4.0, 4.0, 7.0),
-  col2 = c(2.0, 1.0, 1.0, 2.0),
-  col3 = c(3.0, 1.0, 6.0, 6.0)
+  col1 = c(1, 4, 4, 7),
+  col2 = c(2, 1, 1, 2),
+  col3 = c(3, 1, 6, 6)
 )
 
 # testing the input df
@@ -35,20 +35,29 @@ test_that("The input df must be of 'data.frame' type", {
 })
 
 # testing the input strategy
-test_that("The input strategy must be of 'character' type", {
+test_that("The input strategy must be of 'character' type and one of the four options", {
   expect_error(imputer(data, strategy = 2))
+  expect_error(imputer(data, strategy = "test"))
 })
 
-# testing the input strategy
+# testing the input fill_value
 test_that("The input fill_value must be of 'numeric' type or NA", {
   expect_error(imputer(data, fill_value = "2"))
 })
 
+# testing consistency between the input fill_value and strategy
 test_that("The input fill_value and strategy should be consistent.", {
   expect_error(imputer(data, strategy = "mean", fill_value = "2"))
   expect_error(imputer(data, strategy = "constant", fill_value = NA))
 })
 
+
+test_that("The imputed dataframe is not correct", {
+  expect_true(all.equal(imputer(data, strategy = "mean"), imp_mean))
+  expect_true(all.equal(imputer(data, strategy = "median"), imp_median))
+  expect_true(all.equal(imputer(data, strategy = "most_frequent"), imp_most_frequent))
+  expect_true(all.equal(imputer(data, strategy = "constant", 1), imp_constant))
+})
 
 
 
